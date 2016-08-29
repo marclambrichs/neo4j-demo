a0495539c1d11079b03b1ad3111910fff659c4cc
Marc Lambrichs <mlambrichs@melange-it.nl>
2016-08-29 14:49:21 +0200

    Enabled firewall rules for 7474 and 7473

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -5,3 +5,9 @@ Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
 Package {
   allow_virtual => true,
 }
+
+firewall { '100 allow http and https access to neo4j':
+  dport  => [7473, 7474],
+  proto  => tcp,
+  action => accept,
+}
